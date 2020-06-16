ff371714d665c6b705e34af00075946f188c0ee3
Alex G <asgold8@gmail.com>
2020-06-15 20:55:47 -0700

    Grid, filling cell, and random color for each connection.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import http from 'http';
 import path from 'path';
 import express from 'express';
 import socketIo from 'socket.io';
+import randomColor from 'randomcolor';
 
 
 const app = express();
@@ -12,12 +13,14 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on('connection', (sock) => {
+  const color = randomColor();
+  console.log(color);
   //sends to the one connection
   sock.emit('message', 'You are connected :)');
 
   //sends to all connections
-  sock.on('message',(text) => io.emit('message', text));
-  sock.on('turn', ({ x, y }) => io.emit('turn', { x, y }));
+  sock.on('message',(text) => io.emit('message', `${color}: ${text}`));
+  sock.on('turn', ({ x, y }) => io.emit('turn', { x, y, color }));
 });
 
 server.on('error', () => {
